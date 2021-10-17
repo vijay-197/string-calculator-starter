042ed29116f72d10bf1800dcd08d490861693c30
vijay-197 <88487067+vijay-197@users.noreply.github.com>
2021-10-17 11:40:45 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,83 @@
-package calculator;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+	package calculator;
 
-class StringCalculatorShould {
+	import org.junit.jupiter.api.Test;
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	import static org.junit.jupiter.api.Assertions.*;
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
-}
+import org.junit.jupiter.api.BeforeEach;
+
+	class StringCalculatorShould {
+
+		private StringCalculator stringCalculator;
+		
+		@BeforeEach
+	    public void init()
+		{
+			stringCalculator = new StringCalculator();
+		}
+		
+		@Test
+	    public void empty_string_should_return_0() 
+		{
+	        assertEquals(0, stringCalculator.add(""));
+	    }
+	    
+
+	    @Test
+	    public void string_with_single_number_should_return_number_as_int() 
+	    {
+	        assertEquals(1, stringCalculator.add("1"));
+	    }
+	    
+	    
+	    @Test
+	    public void stringHavingTwoNumbers()
+	    {
+	        assertEquals(stringCalculator.add("20,40"),20+40);
+	    }
+	    
+	    @Test
+	    public void StringUnknownNumbers()
+	    {
+	    	assertEquals(10+20+30+60+50, stringCalculator.add("10,20,30,60,50"));
+	    }
+	    
+	    @Test
+	    public void stringWithTwoLinesBetNumbers()
+	    {
+	        assertEquals(9, stringCalculator.add("4\n5"));
+	    }
+	    
+	    @Test
+	    public void stringWithDifferentDelimeter()
+	    {
+	    	assertEquals(3+6+15, stringCalculator.add("//;\n3;6;15") );
+	    }
+	    
+	    @Test
+	    public void stringWithSingleNegativeNoThrowsException()
+	    {
+	    	assertThrows(RuntimeException.class, ()->
+	    		
+	    		stringCalculator.add("7,-6,5")
+	    	);
+	    }
+	    
+	    @Test
+	    public void stringWithMultipleNegativeNoThrowsException()
+	    {
+	    		RuntimeException rex= null;
+	    		try{
+	    			stringCalculator.add("4,7,-8,11,15,-4");
+	    		}catch(RuntimeException e)
+	    		{
+	    			rex=e;
+	    		}
+	    	
+	    	assertNotNull(rex);
+	    	assertEquals("negatives not allowed: [-8, -4]", rex.getMessage());
+	    	
+	    }
+	}
